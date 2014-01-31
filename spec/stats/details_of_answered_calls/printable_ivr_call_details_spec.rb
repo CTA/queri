e1ca67db8ff32f7a547fe5a4c6ac8f7b7efa6968
Brad Rice <bradley_rice@mac.com>
2014-01-30 23:18:10 -0600

    Refactor PrintableIvrCallDetails class, though still not functional

--- spec/stats/details_of_answered_calls/printable_ivr_call_details_spec.rb
+++ spec/stats/details_of_answered_calls/printable_ivr_call_details_spec.rb
@@ -1,4 +1,4 @@
-require File.join( File.dirname(__FILE__), '..', '..', 'spec_helper' )
+require 'spec_helper'
 
 describe Queri::Stats::DetailsOfAnsweredCalls::PrintableIvrCallDetails do
   let(:report_class) { Queri::Stats::DetailsOfAnsweredCalls::PrintableIvrCallDetails }
